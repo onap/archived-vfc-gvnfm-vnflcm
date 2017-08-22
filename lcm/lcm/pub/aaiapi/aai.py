01cf7921c6a3a5cd52bc531f2105b2b66c72755c
ying.yunlong <ying.yunlong@zte.com.cn>
2017-08-22 15:16:34 +0800

    Implement vnflcm call_req_aai function
    As aai rest api is https, add new call_req_aai
    function to implement the rest call to aai.
    commit agagin.
    
    Change-Id: If01790f21e7725f0ba13375a29a9be06d2544899
    Issue-ID: VFC-133
    Signed-off-by: ying.yunlong <ying.yunlong@zte.com.cn>

--- lcm/lcm/pub/aaiapi/aai.py
+++ lcm/lcm/pub/aaiapi/aai.py
@@ -11,6 +11,29 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import json
+import logging
+
+from lcm.pub.config.config import AAI_BASE_URL, AAI_USER, AAI_PASSWORD
+from lcm.pub.exceptions import NFLCMException
+from lcm.pub.utils.restcall import rest_no_auth, call_req_aai
+
+logger = logging.getLogger(__name__)
 
 def call_aai(full_url, method, req_data):
+    pass
+
+def create_ns(ns_id, data):
+    resource = "/service-design-and-creation/services/service/%s" % ns_id
+    ret = call_req_aai(AAI_BASE_URL, AAI_USER, AAI_PASSWORD, rest_no_auth, resource, "PUT", data)
+    if ret[0] != 0:
+        logger.error("Status code is %s, detail is %s.", ret[2], ret[1])
+        raise NFLCMException("Nf instancing apply grant exception")
+    return json.JSONDecoder().decode(ret[1])
+
+def create_vnf(vnf_id, data):
+    # full_url = AAI_BASE_URL + "/network/generic-vnfs/generic-vnf/%s" % vnf_id
+    pass
+
+def create_vserver(vserver_id, data):
     pass
