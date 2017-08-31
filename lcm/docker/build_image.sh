48ef2472147db344545d15213f7a0cf91a5db864
fujinhua <fu.jinhua@zte.com.cn>
2017-08-31 17:47:58 +0800

    Add docker push command for vfc-gvnfm-vnflcm
    Change-Id: Ia400d6d4e6ba698dc5e731194fff13ef11ee510d
    Issue-Id: VFC-203
    Signed-off-by: fujinhua <fu.jinhua@zte.com.cn>

--- lcm/docker/build_image.sh
+++ lcm/docker/build_image.sh
@@ -5,6 +5,7 @@ PROXY_ARGS=""
 ORG="onap"
 VERSION="1.0.0-SNAPSHOT"
 IMAGE="vfc-gvnfm-vnflcm"
+DOCKER_REPOSITORY="nexus3.onap.org:10003"
 
 if [ $HTTP_PROXY ]; then
     PROXY_ARGS+="--build-arg HTTP_PROXY=${HTTP_PROXY}"
@@ -14,8 +15,14 @@ if [ $HTTPS_PROXY ]; then
 fi
 
 function build_vnflcm {
-    docker build ${PROXY_ARGS} --build-arg MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD} -t ${ORG}/${IMAGE}:${VERSION} -t ${ORG}/${IMAGE}:latest ${IMAGE}/target
+    docker build ${PROXY_ARGS} --build-arg MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD} -t ${ORG}/${IMAGE}:${VERSION} -t ${ORG}/${IMAGE}:latest .
+}
+
+function push_vnflcm {
+    docker push ${DOCKER_REPOSITORY}/${ORG}/${IMAGE}:${VERSION}
+    docker push ${DOCKER_REPOSITORY}/${ORG}/${IMAGE}:latest
 }
 
 build_vnflcm
+push_vnflcm
 docker image list
