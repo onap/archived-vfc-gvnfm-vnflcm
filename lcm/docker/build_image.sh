b3c72963f79580bddef8d5b51866134ef1a5ebe7
fujinhua <fu.jinhua@zte.com.cn>
2017-09-01 10:42:22 +0800

    Update image name for vfc-gvnfm-vnflcm
    Change-Id: I328b9697c74746f40e3c3ef4427b73261b48bcf1
    Issue-Id: VFC-212
    Signed-off-by: fujinhua <fu.jinhua@zte.com.cn>

--- lcm/docker/build_image.sh
+++ lcm/docker/build_image.sh
@@ -4,7 +4,8 @@ MYSQL_ROOT_PASSWORD="root"
 PROXY_ARGS=""
 ORG="onap"
 VERSION="1.0.0-SNAPSHOT"
-IMAGE="vfc-gvnfm-vnflcm"
+PROJECT="vfc"
+IMAGE="vnflcm"
 DOCKER_REPOSITORY="nexus3.onap.org:10003"
 
 if [ $HTTP_PROXY ]; then
@@ -15,12 +16,12 @@ if [ $HTTPS_PROXY ]; then
 fi
 
 function build_vnflcm {
-    docker build ${PROXY_ARGS} --build-arg MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD} -t ${ORG}/${IMAGE}:${VERSION} -t ${ORG}/${IMAGE}:latest .
+    docker build ${PROXY_ARGS} --build-arg MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD} -t ${ORG}/${PROJECT}/${IMAGE}:${VERSION} -t ${ORG}/${PROJECT}/${IMAGE}:latest .
 }
 
 function push_vnflcm {
-    docker push ${DOCKER_REPOSITORY}/${ORG}/${IMAGE}:${VERSION}
-    docker push ${DOCKER_REPOSITORY}/${ORG}/${IMAGE}:latest
+    docker push ${DOCKER_REPOSITORY}/${ORG}/${PROJECT}/${IMAGE}:${VERSION}
+    docker push ${DOCKER_REPOSITORY}/${ORG}/${PROJECT}/${IMAGE}:latest
 }
 
 build_vnflcm
