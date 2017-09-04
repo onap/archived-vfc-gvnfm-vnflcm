7d1221edf3d778ca3127b53c9fe5c765c8d45bf4
fujinhua <fu.jinhua@zte.com.cn>
2017-09-04 15:46:48 +0800

    Fix log file name of vfc-gvnfm-vnflcm
    Change-Id: Ia4810de89206c3d8fd9ce7db105d978e2ff96519
    Issue-Id: VFC-261
    Signed-off-by: fujinhua <fu.jinhua@zte.com.cn>

--- lcm/docker/instance_run.sh
+++ lcm/docker/instance_run.sh
@@ -3,7 +3,7 @@ cd /service/vfc/gvnfm/vnflcm/lcm
 chmod +x run.sh
 ./run.sh
 
-while [ ! -f logs/gvnfm_vnflcm.log ]; do
+while [ ! -f logs/runtime_lcm.log ]; do
     sleep 1
 done
-tail -F logs/gvnfm_vnflcm.log
+tail -F logs/runtime_lcm.log
