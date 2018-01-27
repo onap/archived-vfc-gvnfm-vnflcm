1cce02e84e07f070730785c63dbfccaa498654e7
ying.yunlong <ying.yunlong@zte.com.cn>
2018-01-27 16:32:47 +0800

    Add vfc-vnflcm getVnfs schema
    Change-Id: Id785a6d1acf1073b2ad5605e3ac2839066dbc0e7
    Issue-ID: VFC-682
    Signed-off-by: ying.yunlong <ying.yunlong@zte.com.cn>

--- lcm/lcm/nf/vnfs/serializers.py
+++ lcm/lcm/nf/vnfs/serializers.py
@@ -23,3 +23,59 @@ class CreateVnfReqSerializer(serializers.Serializer):
 
 class CreateVnfRespSerializer(serializers.Serializer):
     vnfInstanceId = serializers.CharField(help_text="vnf Instance Id", required=True)
+
+
+class virtualStorageResourceInfoSerializer(serializers.Serializer):
+    pass
+
+
+class virtualLinkResourceInfoSerializer(serializers.Serializer):
+    pass
+
+
+class vnfcResourceInfoSerializer(serializers.Serializer):
+    pass
+
+
+class vimInfoSerializer(serializers.Serializer):
+    pass
+
+
+class monitoringParametersSerializer(serializers.Serializer):
+    pass
+
+
+class ExtVirtualLinkInfoSerializer(serializers.Serializer):
+    pass
+
+
+class extCpInfoSerializer(serializers.Serializer):
+    pass
+
+
+class ScaleInfoSerializer(serializers.Serializer):
+    pass
+
+
+class instantiatedVnfInfoSerializer(serializers.Serializer):
+    flavourId = serializers.CharField(help_text="flavour Id", required=True)
+    vnfState = serializers.ChoiceField(help_text="vnf State", choices=['STARTED', 'STOPPED'], required=True)
+    scaleStatus = ScaleInfoSerializer(help_text="scaleStatus", many=True)
+    extCpInfo = extCpInfoSerializer(help_text="extCpInfo", many=True)
+    extVirtualLink = ExtVirtualLinkInfoSerializer(help_text="extVirtualLink", many=True)
+    monitoringParameters = monitoringParametersSerializer(help_text="monitoringParameters", many=True, allow_null=True)
+    vimInfo = vimInfoSerializer(help_text="vimInfo", many=True)
+    vnfcResourceInfo = vnfcResourceInfoSerializer(help_text="vnfcResourceInfo", many=True)
+    virtualLinkResourceInfo = virtualLinkResourceInfoSerializer(help_text="virtualLinkResourceInfo", many=True)
+    virtualStorageResourceInfo = virtualStorageResourceInfoSerializer(help_text="virtualStorageResourceInfo", many=True)
+    pass
+
+
+class VnfsInfoSerializer(serializers.ListSerializer):
+    vnfInstanceId = serializers.CharField(help_text="vnf Instance Id", required=True)
+    vnfInstanceName = serializers.CharField(help_text="vnf Instance Name", required=True)
+    onboardedVnfPkgInfoId = serializers.CharField(help_text="onboarded Vnf Pkg Info Id", required=False,
+                                                  allow_null=True)
+    vnfdVersion = serializers.CharField(help_text="vnfd Version", required=False, allow_null=True)
+    vnfProvider = serializers.CharField(help_text="vnf Provider", required=False, allow_null=True)
+    instantiatedVnfInfo = instantiatedVnfInfoSerializer(help_text="instantiatedVnfInfo", required=True)
