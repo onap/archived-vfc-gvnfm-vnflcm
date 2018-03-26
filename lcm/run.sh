833f4a239be750e0ec8faeb0fa6400fc69a04134
ying.yunlong <ying.yunlong@zte.com.cn>
2018-03-26 14:41:44 +0800

    Modify vfc-vnflcm log export directory
    Change-Id: I69350b31c16757ebeaeac9fa4f467b5462616071
    Issue-ID: VFC-839
    Signed-off-by: ying.yunlong <ying.yunlong@zte.com.cn>

--- lcm/run.sh
+++ lcm/run.sh
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-logDir="/var/log/onap/vfc-gvnfm-vnflcm/"
+logDir="/var/log/onap/vfc/gvnfm-vnflcm/"
 if [ ! -x  $logDir  ]; then
        mkdir -p $logDir
 fi
