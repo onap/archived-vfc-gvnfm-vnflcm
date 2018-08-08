be8a3ac8d84b2bffd32d5d5e5f2609706e93d33d
fujinhua <fu.jinhua@zte.com.cn>
2018-08-08 15:13:25 +0800

    Remove DB and redis service from vnflcm
    Change-Id: I83f9bc8b0ccaaaad1f04a9590c1bfba43ce5be51
    Issue-ID: VFC-979
    Signed-off-by: fujinhua <fu.jinhua@zte.com.cn>

--- lcm/docker/instance_config.sh
+++ lcm/docker/instance_config.sh
@@ -11,10 +11,6 @@ if [ $MSB_PORT ]; then
     sed -i "s|MSB_SERVICE_PORT.*|MSB_SERVICE_PORT = '$MSB_PORT'|" vfc/gvnfm/vnflcm/lcm/lcm/pub/config/config.py
 fi
 
-sed -i "s|DB_NAME.*|DB_NAME = 'gvnfm'|" vfc/gvnfm/vnflcm/lcm/lcm/pub/config/config.py
-sed -i "s|DB_USER.*|DB_USER = 'gvnfm'|" vfc/gvnfm/vnflcm/lcm/lcm/pub/config/config.py
-sed -i "s|DB_PASSWD.*|DB_PASSWD = 'gvnfm'|" vfc/gvnfm/vnflcm/lcm/lcm/pub/config/config.py
-
 if [ $SERVICE_IP ]; then
     sed -i "s|\"ip\": \".*\"|\"ip\": \"$SERVICE_IP\"|" vfc/gvnfm/vnflcm/lcm/lcm/pub/config/config.py
 fi
@@ -22,16 +18,12 @@ fi
 sed -i "s/127.0.0.1:80/$MSB_IP:$MSB_PORT/" vfc/gvnfm/vnflcm/lcm/lcm/pub/config/config.py
 
 # Configure MYSQL
-if [ -z "$MYSQL_ADDR" ]; then
-    export MYSQL_IP=`hostname -i`
-    export MYSQL_PORT=3306
-    export MYSQL_ADDR=$MYSQL_IP:$MYSQL_PORT
-else
-    MYSQL_IP=`echo $MYSQL_ADDR | cut -d: -f 1`
-    MYSQL_PORT=`echo $MYSQL_ADDR | cut -d: -f 2`
-fi
+MYSQL_IP=`echo $MYSQL_ADDR | cut -d: -f 1`
+MYSQL_PORT=`echo $MYSQL_ADDR | cut -d: -f 2`
 echo "MYSQL_ADDR=$MYSQL_ADDR"
+
 sed -i "s|DB_IP.*|DB_IP = '$MYSQL_IP'|" vfc/gvnfm/vnflcm/lcm/lcm/pub/config/config.py
 sed -i "s|DB_PORT.*|DB_PORT = $MYSQL_PORT|" vfc/gvnfm/vnflcm/lcm/lcm/pub/config/config.py
+sed -i "s|REDIS_HOST.*|REDIS_HOST = '$MYSQL_IP'|" vfc/gvnfm/vnflcm/lcm/lcm/pub/config/config.py
 
 cat vfc/gvnfm/vnflcm/lcm/lcm/pub/config/config.py
