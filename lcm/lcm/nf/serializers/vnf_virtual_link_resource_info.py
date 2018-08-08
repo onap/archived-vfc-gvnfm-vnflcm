f5f62b096d35a17db4065ae32340f694ff7087cd
laili <lai.li@zte.com.cn>
2018-08-08 11:11:04 +0800

    Modify vnf query related stuffs in vnflcm.
    Add virtual_storage_resource_info.py and vnf_virtual_link_resource_inf.py which is used by instantiated_vnf_info.py.
    
    Change-Id: I94962c191936060dbe0da0c03a12cfbdd8515a43
    Issue-ID: VFC-1016
    Signed-off-by: laili <lai.li@zte.com.cn>

---
+++ lcm/lcm/nf/serializers/vnf_virtual_link_resource_info.py
@@ -0,0 +1,53 @@
+# Copyright 2018 ZTE Corporation.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from rest_framework import serializers
+from resource_handle import ResourceHandleSerializer
+from vnf_link_port_info import VnfLinkPortInfoSerializer
+
+
+class VnfVirtualLinkResourceInfoSerializer(serializers.Serializer):
+    id = serializers.CharField(
+        help_text="Identifier of this VnfVirtualLinkResourceInfo instance.",
+        max_length=255,
+        required=True,
+        allow_null=False,
+        allow_blank=False)
+    virtualLinkDescId = serializers.CharField(
+        help_text="Identifier of the VNF Virtual Link Descriptor (VLD) in the VNFD.",
+        max_length=255,
+        required=True,
+        allow_null=False,
+        allow_blank=False)
+    networkResource = ResourceHandleSerializer(
+        help_text="Reference to the VirtualNetwork resource.",
+        required=True,
+        allow_null=False)
+    reservationId = serializers.CharField(
+        help_text="The reservation identifier applicable to the resource.",
+        max_length=255,
+        required=False,
+        allow_null=True,
+        allow_blank=True)
+    vnfLinkPorts = VnfLinkPortInfoSerializer(
+        help_text="Links ports of this VL. \
+        Shall be present when the linkPort is used for external connectivity by the VNF",
+        many=True,
+        required=False,
+        allow_null=True)
+    metadata = serializers.DictField(
+        help_text="Metadata about this resource.",
+        child=serializers.CharField(help_text="KeyValue Pairs", allow_blank=True),
+        required=False,
+        allow_null=True)
