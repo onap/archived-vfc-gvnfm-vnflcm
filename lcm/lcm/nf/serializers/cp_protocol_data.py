ece40bf543e86cf245460bd4ac7bacdd5c1dede7
laili <lai.li@zte.com.cn>
2018-08-09 11:33:12 +0800

    Modify vnf instantiation related stuffs.
    Add ip_over_ethernet_address_data.py used by cp_protocol_data.py to serializers.
    Add cp_protocol_data.py used by vnf_ext_cp_config.py to serializer.
    
    Change-Id: Ic0348306ee8e182fab56d82acb72af8774d0feae
    Issue-ID: VFC-1017
    Signed-off-by: laili <lai.li@zte.com.cn>

---
+++ lcm/lcm/nf/serializers/cp_protocol_data.py
@@ -0,0 +1,29 @@
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from rest_framework import serializers
+
+from ip_over_ethernet_address_data import IpOverEthernetAddressDataSerializer
+
+
+class CpProtocolDataSerializer(serializers.Serializer):
+    layerProtocol = serializers.ChoiceField(
+        help_text="Identifier of layer(s) and protocol(s).",
+        choices=["IP_OVER_ETHERNET"],
+        required=True,
+        allow_null=False,
+        allow_blank=False)
+    ipOverEthernet = IpOverEthernetAddressDataSerializer(
+        help_text="Network address data for IP over Ethernet to assign to the extCP instance. \
+        Shall be present if layerProtocol is equal to IP_OVER_ETHERNET, \
+        and shall be absent otherwise.",
+        required=False,
+        allow_null=True,)
