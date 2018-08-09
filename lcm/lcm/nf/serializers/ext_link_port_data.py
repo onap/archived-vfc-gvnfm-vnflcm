fb2e3386b68226a54eacd021e1c3b398da19aa4b
laili <lai.li@zte.com.cn>
2018-08-09 11:51:32 +0800

    Modify vnf instantiation related stuffs.
    Add vnf_ext_cp_data.py and ext_link_port_data.py used by ext_virtual_link_data.py to serializers.
    
    Change-Id: I96df71714b0c548cf5e3d55ffcaa9746844ea891
    Issue-ID: VFC-1017
    Signed-off-by: laili <lai.li@zte.com.cn>

---
+++ lcm/lcm/nf/serializers/ext_link_port_data.py
@@ -0,0 +1,30 @@
+# Copyright 2018 ZTE Corporation.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from rest_framework import serializers
+
+from resource_handle import ResourceHandleSerializer
+
+
+class ExtLinkPortDataSerializer(serializers.Serializer):
+    id = serializers.CharField(
+        help_text="Identifier of this link port as provided by the entity that has created the link port.",
+        required=True,
+        allow_null=False,
+        allow_blank=False)
+    resourceHandle = ResourceHandleSerializer(
+        help_text="Reference to the virtualised resource realizing this link port.",
+        required=True,
+        allow_null=False,
+        allow_blank=False)
