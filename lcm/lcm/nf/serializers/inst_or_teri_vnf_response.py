fe640a9f57f0d1ef0f6525c25a6deb497c11d5bf
laili <lai.li@zte.com.cn>
2018-08-09 20:45:10 +0800

    Modify vnf instantiation related stuffs.
    Add inst_or_teri_vnf_response.py for vnf instantiation.
    Correct minor errors in ext_link_port_data.py and instantiate_vnf_request.py.
    
    Change-Id: I22f28b62132f8d1147cfa467041f49f38977a274
    Issue-ID: VFC-1017
    Signed-off-by: laili <lai.li@zte.com.cn>

--- lcm/lcm/nf/serializers/inst_or_teri_vnf_response.py
+++ lcm/lcm/nf/serializers/inst_or_teri_vnf_response.py
@@ -1,4 +1,4 @@
-# Copyright 2018 ZTE Corporation.
+# Copyright 2017 ZTE Corporation.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,18 +12,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
 from rest_framework import serializers
 
-from resource_handle import ResourceHandleSerializer
 
-
-class ExtLinkPortDataSerializer(serializers.Serializer):
-    id = serializers.CharField(
-        help_text="Identifier of this link port as provided by the entity that has created the link port.",
+class InstOrTeriVnfResponseSerializer(serializers.Serializer):
+    jobId = serializers.CharField(
+        help_text="jobId",
         required=True,
-        allow_null=False,
-        allow_blank=False)
-    resourceHandle = ResourceHandleSerializer(
-        help_text="Reference to the virtualised resource realizing this link port.",
-        required=True,
-        allow_null=False)
+        max_length=255,
+        allow_null=True)
