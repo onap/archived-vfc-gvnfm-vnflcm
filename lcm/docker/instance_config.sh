e7b11d87ae31049716475b54b8eb9a0aa92650a4
yangyan <yangyanyj@chinamobile.com>
2019-03-26 10:27:58 +0800

    update redis service nname
    Change-Id: Idb026a32fd80c3ae62ddd5e4c85b0a65ea86bee6
    Issue-ID: VFC-1316
    Signed-off-by: yangyan <yangyanyj@chinamobile.com>

--- lcm/docker/instance_config.sh
+++ lcm/docker/instance_config.sh
@@ -24,9 +24,13 @@ echo "MYSQL_ADDR=$MYSQL_ADDR"
 MYSQL_USER=`echo $MYSQL_AUTH | cut -d: -f 1`
 MYSQL_ROOT_PASSWORD=`echo $MYSQL_AUTH | cut -d: -f 2`
 
+REDIS_IP=`echo $REDIS_ADDR | cut -d: -f 1`
+REDIS_PORT=`echo $REDIS_ADDR | cut -d: -f 2`
+
 sed -i "s|DB_IP.*|DB_IP = '$MYSQL_IP'|" vfc/gvnfm/vnflcm/lcm/lcm/pub/config/config.py
 sed -i "s|DB_PORT.*|DB_PORT = $MYSQL_PORT|" vfc/gvnfm/vnflcm/lcm/lcm/pub/config/config.py
-sed -i "s|REDIS_HOST.*|REDIS_HOST = '$MYSQL_IP'|" vfc/gvnfm/vnflcm/lcm/lcm/pub/config/config.py
+sed -i "s|REDIS_HOST.*|REDIS_HOST = '$REDIS_IP'|" vfc/gvnfm/vnflcm/lcm/lcm/pub/config/config.py
+sed -i "s|REDIS_HOST.*|REDIS_HOST = '$REDIS_PORT'|" vfc/gvnfm/vnflcm/lcm/lcm/pub/config/config.py
 sed -i "s|DB_USER.*|DB_USER = '$MYSQL_USER'|" vfc/gvnfm/vnflcm/lcm/lcm/pub/config/config.py
 sed -i "s|DB_PASSWD.*|DB_PASSWD = '$MYSQL_ROOT_PASSWORD'|" vfc/gvnfm/vnflcm/lcm/lcm/pub/config/config.py
 
