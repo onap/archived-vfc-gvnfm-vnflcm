29b2066940c6b38af8a0a6b43ddc35ca07bd3d94
fujinhua <fu.jinhua@zte.com.cn>
2019-04-05 17:17:06 +0800

    Refactor codes for heal vnf
    Change-Id: Ic42a459ef37e34fd742f8439dee91e6ed295072b
    Issue-ID: VFC-1306
    Signed-off-by: fujinhua <fu.jinhua@zte.com.cn>

--- lcm/lcm/nf/views/heal_vnf_view.py
+++ lcm/lcm/nf/views/heal_vnf_view.py
@@ -13,7 +13,6 @@
 # limitations under the License.
 
 import logging
-import traceback
 
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework import status
@@ -23,10 +22,11 @@ from rest_framework.views import APIView
 from lcm.nf.biz.heal_vnf import HealVnf
 from lcm.nf.serializers.heal_vnf_req import HealVnfRequestSerializer
 from lcm.nf.serializers.response import ProblemDetailsSerializer
-from lcm.pub.exceptions import NFLCMException, NFLCMExceptionNotFound, NFLCMExceptionConflict
+from lcm.pub.exceptions import NFLCMException, NFLCMExceptionNotFound
 from lcm.pub.utils.jobutil import JobUtil
 from lcm.pub.database.models import NfInstModel
 from lcm.nf.const import VNF_STATUS
+from .common import view_safe_call_with_log
 
 logger = logging.getLogger(__name__)
 
@@ -41,40 +41,22 @@ class HealVnfView(APIView):
             status.HTTP_500_INTERNAL_SERVER_ERROR: "Internal error"
         }
     )
+    @view_safe_call_with_log(logger=logger)
     def post(self, request, instanceid):
         logger.debug("HealVnf--post::> %s" % request.data)
-        try:
-            heal_vnf_request_serializer = HealVnfRequestSerializer(data=request.data)
-            if not heal_vnf_request_serializer.is_valid():
-                raise NFLCMException(heal_vnf_request_serializer.errors)
 
-            job_id = JobUtil.create_job('NF', 'HEAL', instanceid)
-            JobUtil.add_job_status(job_id, 0, "HEAL_VNF_READY")
-            self.heal_pre_check(instanceid, job_id)
-            HealVnf(heal_vnf_request_serializer.data, instanceid, job_id).start()
-            response = Response(data={"jobId": job_id}, status=status.HTTP_202_ACCEPTED)
-            # todo, heal_vnf codes uses job as the status storage, not in VNFLcmOpOccModel.
-            # response["Location"] = "/api/vnflcm/v1/vnf_lc_ops/%s" % lcmopoccid
-            return response
-        except NFLCMExceptionNotFound as e:
-            probDetail = ProblemDetailsSerializer(data={"status": status.HTTP_404_NOT_FOUND, "detail": "VNF Instance not found"})
-            resp_isvalid = probDetail.is_valid()
-            if not resp_isvalid:
-                raise NFLCMException(probDetail.errors)
-            return Response(data=probDetail.data, status=status.HTTP_404_NOT_FOUND)
-        except NFLCMExceptionConflict as e:
-            probDetail = ProblemDetailsSerializer(data={"status": status.HTTP_409_CONFLICT, "detail": "VNF Instance not in Instantiated State"})
-            resp_isvalid = probDetail.is_valid()
-            if not resp_isvalid:
-                raise NFLCMException(probDetail.errors)
-            return Response(data=probDetail.data, status=status.HTTP_409_CONFLICT)
-        except NFLCMException as e:
-            logger.error(e.message)
-            return Response(data={'error': '%s' % e.message}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-        except Exception as e:
-            logger.error(e.message)
-            logger.error(traceback.format_exc())
-            return Response(data={'error': 'unexpected exception'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        heal_vnf_request_serializer = HealVnfRequestSerializer(data=request.data)
+        if not heal_vnf_request_serializer.is_valid():
+            raise NFLCMException(heal_vnf_request_serializer.errors)
+
+        job_id = JobUtil.create_job('NF', 'HEAL', instanceid)
+        JobUtil.add_job_status(job_id, 0, "HEAL_VNF_READY")
+        self.heal_pre_check(instanceid, job_id)
+        HealVnf(heal_vnf_request_serializer.data, instanceid, job_id).start()
+        response = Response(data={"jobId": job_id}, status=status.HTTP_202_ACCEPTED)
+        # todo, heal_vnf codes uses job as the status storage, not in VNFLcmOpOccModel.
+        # response["Location"] = "/api/vnflcm/v1/vnf_lc_ops/%s" % lcmopoccid
+        return response
 
     def heal_pre_check(self, nf_inst_id, job_id):
         vnf_insts = NfInstModel.objects.filter(nfinstid=nf_inst_id)
