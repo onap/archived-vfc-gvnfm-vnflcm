837b609926ede4bbb8dbcd5b4af78ecf321a2db6
fujinhua <fu.jinhua@zte.com.cn>
2019-04-06 09:00:21 +0800

    Refactor codes for scale vnf
    Change-Id: I2d3934b3f3c34c19b4ec832363d2f4eb90b5a7df
    Issue-ID: VFC-1306
    Signed-off-by: fujinhua <fu.jinhua@zte.com.cn>

--- lcm/lcm/nf/views/scale_vnf_view.py
+++ lcm/lcm/nf/views/scale_vnf_view.py
@@ -22,11 +22,14 @@ from rest_framework.views import APIView
 
 from lcm.nf.serializers.scale_vnf_request import ScaleVnfRequestSerializer
 from lcm.nf.serializers.response import ProblemDetailsSerializer
-from lcm.pub.exceptions import NFLCMException, NFLCMExceptionNotFound, NFLCMExceptionConflict
+from lcm.pub.exceptions import NFLCMException
+from lcm.pub.exceptions import NFLCMExceptionNotFound
+from lcm.pub.exceptions import NFLCMExceptionConflict
 from lcm.pub.utils.jobutil import JobUtil
 from lcm.pub.database.models import NfInstModel
 from lcm.nf.const import VNF_STATUS
 from lcm.nf.biz.scale_vnf import ScaleVnf
+from .common import view_safe_call_with_log
 
 logger = logging.getLogger(__name__)
 
@@ -43,50 +46,28 @@ class ScaleVnfView(APIView):
     )
     def post(self, request, instanceid):
         logger.debug("ScaleVnf--post::> %s" % request.data)
-        try:
-            scale_vnf_request_serializer = ScaleVnfRequestSerializer(data=request.data)
-            if not scale_vnf_request_serializer.is_valid():
-                raise NFLCMException(scale_vnf_request_serializer.errors)
 
-            job_id = JobUtil.create_job('NF', 'SCALE', instanceid)
-            JobUtil.add_job_status(job_id, 0, "SCALE_VNF_READY")
-            self.scale_pre_check(instanceid, job_id)
+        scale_vnf_request_serializer = ScaleVnfRequestSerializer(data=request.data)
+        if not scale_vnf_request_serializer.is_valid():
+            raise NFLCMException(scale_vnf_request_serializer.errors)
 
-            ScaleVnf(scale_vnf_request_serializer.data, instanceid, job_id).start()
+        job_id = JobUtil.create_job('NF', 'SCALE', instanceid)
+        JobUtil.add_job_status(job_id, 0, "SCALE_VNF_READY")
+        self.scale_pre_check(instanceid, job_id)
 
-            response = Response(data={"jobId": job_id},
-                                status=status.HTTP_202_ACCEPTED)
-            return response
-        except NFLCMExceptionNotFound as e:
-            probDetail = ProblemDetailsSerializer(data={"status": status.HTTP_404_NOT_FOUND,
-                                                        "detail": "VNF Instance not found"})
-            resp_isvalid = probDetail.is_valid()
-            if not resp_isvalid:
-                raise NFLCMException(probDetail.errors)
-            return Response(data=probDetail.data,
-                            status=status.HTTP_404_NOT_FOUND)
-        except NFLCMExceptionConflict as e:
-            probDetail = ProblemDetailsSerializer(data={"status": status.HTTP_409_CONFLICT,
-                                                        "detail": "VNF Instance not in Instantiated State"})
-            resp_isvalid = probDetail.is_valid()
-            if not resp_isvalid:
-                raise NFLCMException(probDetail.errors)
-            return Response(data=probDetail.data,
-                            status=status.HTTP_409_CONFLICT)
-        except NFLCMException as e:
-            logger.error(e.message)
-            return Response(data={'error': '%s' % e.message},
-                            status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-        except Exception as e:
-            logger.error(e.message)
-            logger.error(traceback.format_exc())
-            return Response(data={'error': 'unexpected exception'},
-                            status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        ScaleVnf(scale_vnf_request_serializer.data, instanceid, job_id).start()
+
+        response = Response(data={"jobId": job_id},
+                            status=status.HTTP_202_ACCEPTED)
+        return response
 
     def scale_pre_check(self, nf_inst_id, job_id):
         vnf_insts = NfInstModel.objects.filter(nfinstid=nf_inst_id)
         if not vnf_insts.exists():
             raise NFLCMExceptionNotFound("VNF nf_inst_id does not exist.")
+
+        if vnf_insts[0].status != 'INSTANTIATED':
+            raise NFLCMExceptionConflict("VNF instantiationState is not INSTANTIATED.")
 
         vnf_insts.update(status=VNF_STATUS.SCALING)
         JobUtil.add_job_status(job_id, 15, 'Nf scaling pre-check finish')
