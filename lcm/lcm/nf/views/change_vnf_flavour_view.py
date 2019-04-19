29754d3395f30da69c10687d58493ef257f756fc
fujinhua <fu.jinhua@zte.com.cn>
2019-04-19 12:02:28 +0800

    refactor codes for chg vnf flavour
    Change-Id: Ifeab1b0ca389218c25804020a6a188dcaedec54a
    Issue-ID: VFC-1307
    Signed-off-by: fujinhua <fu.jinhua@zte.com.cn>

--- lcm/lcm/nf/views/change_vnf_flavour_view.py
+++ lcm/lcm/nf/views/change_vnf_flavour_view.py
@@ -16,19 +16,15 @@ import logging
 
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework import status
-from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from lcm.nf.serializers.change_vnf_flavour_request import ChangeVnfFlavourRequestSerializer
 from lcm.nf.serializers.response import ProblemDetailsSerializer
-from lcm.pub.exceptions import NFLCMException
-from lcm.pub.exceptions import NFLCMExceptionNotFound
-from lcm.pub.exceptions import NFLCMExceptionConflict
-from lcm.pub.utils.jobutil import JobUtil
-from lcm.pub.database.models import NfInstModel
 from lcm.nf.const import VNF_STATUS
+from lcm.nf.const import OPERATION_TYPE
 from lcm.nf.biz.change_vnf_flavour import ChangeVnfFlavour
 from .common import view_safe_call_with_log
+from .common import deal_vnf_action
 
 logger = logging.getLogger(__name__)
 
@@ -45,30 +41,12 @@ class ChangeVnfFlavourView(APIView):
     )
     @view_safe_call_with_log(logger=logger)
     def post(self, request, instanceid):
-        logger.debug("ChangeVnfFlavour--post::> %s" % request.data)
-
-        chg_flavour_serializer = ChangeVnfFlavourRequestSerializer(data=request.data)
-        if not chg_flavour_serializer.is_valid():
-            raise NFLCMException(chg_flavour_serializer.errors)
-
-        job_id = JobUtil.create_job('NF', 'CHG_VNF_FLAVOUR', instanceid)
-        JobUtil.add_job_status(job_id, 0, "CHG_VNF_FLAVOUR_READY")
-        self.chg_flavour_pre_check(instanceid, job_id)
-
-        ChangeVnfFlavour(chg_flavour_serializer.data, instanceid, job_id).start()
-
-        response = Response(data={"jobId": job_id},
-                            status=status.HTTP_202_ACCEPTED)
-        return response
-
-    def chg_flavour_pre_check(self, nf_inst_id, job_id):
-        vnf_insts = NfInstModel.objects.filter(nfinstid=nf_inst_id)
-        if not vnf_insts.exists():
-            raise NFLCMExceptionNotFound("VNF nf_inst_id does not exist.")
-
-        if vnf_insts[0].status != 'INSTANTIATED':
-            raise NFLCMExceptionConflict("VNF instantiationState is not INSTANTIATED.")
-
-        vnf_insts.update(status=VNF_STATUS.UPDATING)
-        JobUtil.add_job_status(job_id, 15, 'Nf change vnf flavour pre-check finish')
-        logger.info("Nf change vnf flavour pre-check finish")
+        return deal_vnf_action(
+            logger=logger,
+            opt_type=OPERATION_TYPE.CHANGE_FLAVOUR,
+            opt_status=VNF_STATUS.UPDATING,
+            instid=instanceid,
+            req=request,
+            req_serializer=ChangeVnfFlavourRequestSerializer,
+            act_task=ChangeVnfFlavour
+        )
