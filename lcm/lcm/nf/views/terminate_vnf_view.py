5a19d31c7e350aadfc8332792b2555bc46a9f019
fujinhua <fu.jinhua@zte.com.cn>
2019-04-19 15:14:35 +0800

    refactor codes for terminate vnf
    Change-Id: Ia1c5f722109d44345ec0ec854ae114bfc6fa2e42
    Issue-ID: VFC-1307
    Signed-off-by: fujinhua <fu.jinhua@zte.com.cn>

--- lcm/lcm/nf/views/terminate_vnf_view.py
+++ lcm/lcm/nf/views/terminate_vnf_view.py
@@ -16,19 +16,15 @@ import logging
 
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework import status
-from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from lcm.nf.biz.terminate_vnf import TerminateVnf
 from lcm.nf.serializers.terminate_vnf_req import TerminateVnfRequestSerializer
 from lcm.nf.serializers.job_identifier import JobIdentifierSerializer
-from lcm.pub.database.models import NfInstModel
-from lcm.pub.exceptions import NFLCMException
-from lcm.pub.exceptions import NFLCMExceptionConflict
-from lcm.pub.exceptions import NFLCMExceptionNotFound
-from lcm.pub.utils.jobutil import JobUtil
 from lcm.nf.const import VNF_STATUS
+from lcm.nf.const import OPERATION_TYPE
 from .common import view_safe_call_with_log
+from .common import deal_vnf_action
 
 logger = logging.getLogger(__name__)
 
@@ -43,32 +39,12 @@ class TerminateVnfView(APIView):
     )
     @view_safe_call_with_log(logger=logger)
     def post(self, request, instanceid):
-        logger.debug("TerminateVnf--post::> %s" % request.data)
-
-        terminate_vnf_request_serializer = TerminateVnfRequestSerializer(data=request.data)
-        if not terminate_vnf_request_serializer.is_valid():
-            raise NFLCMException(terminate_vnf_request_serializer.errors)
-
-        job_id = JobUtil.create_job('NF', 'TERMINATE', instanceid)
-        JobUtil.add_job_status(job_id, 0, "TERM_VNF_READY")
-
-        self.terminate_pre_check(instanceid, job_id)
-        TerminateVnf(terminate_vnf_request_serializer.data, instanceid, job_id).start()
-
-        terminate_vnf_response_serializer = JobIdentifierSerializer(data={"jobId": job_id})
-        if not terminate_vnf_response_serializer.is_valid():
-            raise NFLCMException(terminate_vnf_response_serializer.errors)
-
-        return Response(data=terminate_vnf_response_serializer.data, status=status.HTTP_202_ACCEPTED)
-
-    def terminate_pre_check(self, nf_inst_id, job_id):
-        vnf_insts = NfInstModel.objects.filter(nfinstid=nf_inst_id)
-        if not vnf_insts.exists():
-            raise NFLCMExceptionNotFound("VNF nf_inst_id does not exist.")
-
-        if vnf_insts[0].status != 'INSTANTIATED':
-            raise NFLCMExceptionConflict("VNF instantiationState is not INSTANTIATED.")
-
-        vnf_insts.update(status=VNF_STATUS.TERMINATING)
-        JobUtil.add_job_status(job_id, 15, 'Nf terminating pre-check finish')
-        logger.info("Nf terminating pre-check finish")
+        return deal_vnf_action(
+            logger=logger,
+            opt_type=OPERATION_TYPE.TERMINATE,
+            opt_status=VNF_STATUS.TERMINATING,
+            instid=instanceid,
+            req=request,
+            req_serializer=TerminateVnfRequestSerializer,
+            act_task=TerminateVnf
+        )
