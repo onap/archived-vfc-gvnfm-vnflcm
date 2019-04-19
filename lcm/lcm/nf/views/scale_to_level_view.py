9a791938ac01cb15f90db752e1fb4ca688e66eec
fujinhua <fu.jinhua@zte.com.cn>
2019-04-19 14:07:21 +0800

    refactor codes for scale to level
    Change-Id: I4bb930d381d57342d5f188e42e3649c1b106f7e6
    Issue-ID: VFC-1307
    Signed-off-by: fujinhua <fu.jinhua@zte.com.cn>

--- lcm/lcm/nf/views/scale_to_level_view.py
+++ lcm/lcm/nf/views/scale_to_level_view.py
@@ -16,19 +16,15 @@ import logging
 
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework import status
-from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from lcm.nf.serializers.scale_vnf_to_level_request_serializer import ScaleVnfToLevelRequestSerializer
 from lcm.nf.serializers.response import ProblemDetailsSerializer
-from lcm.pub.exceptions import NFLCMException
-from lcm.pub.exceptions import NFLCMExceptionNotFound
-from lcm.pub.exceptions import NFLCMExceptionConflict
-from lcm.pub.utils.jobutil import JobUtil
-from lcm.pub.database.models import NfInstModel
-from lcm.nf.const import VNF_STATUS
 from lcm.nf.biz.scale_vnf_to_level import ScaleVnfToLevel
+from lcm.nf.const import VNF_STATUS
+from lcm.nf.const import OPERATION_TYPE
 from .common import view_safe_call_with_log
+from .common import deal_vnf_action
 
 logger = logging.getLogger(__name__)
 
@@ -45,30 +41,12 @@ class ScaleVnfToLevelView(APIView):
     )
     @view_safe_call_with_log(logger=logger)
     def post(self, request, instanceid):
-        logger.debug("ScaleVnfToLevel--post::> %s" % request.data)
-
-        scale_to_level_serializer = ScaleVnfToLevelRequestSerializer(data=request.data)
-        if not scale_to_level_serializer.is_valid():
-            raise NFLCMException(scale_to_level_serializer.errors)
-
-        job_id = JobUtil.create_job('NF', 'SCALE_TO_LEVEL', instanceid)
-        JobUtil.add_job_status(job_id, 0, "SCALE_VNF_TO_LEVEL_READY")
-        self.scale_pre_check(instanceid, job_id)
-
-        ScaleVnfToLevel(scale_to_level_serializer.data, instanceid, job_id).start()
-
-        response = Response(data={"jobId": job_id},
-                            status=status.HTTP_202_ACCEPTED)
-        return response
-
-    def scale_pre_check(self, nf_inst_id, job_id):
-        vnf_insts = NfInstModel.objects.filter(nfinstid=nf_inst_id)
-        if not vnf_insts.exists():
-            raise NFLCMExceptionNotFound("VNF nf_inst_id does not exist.")
-
-        if vnf_insts[0].status != 'INSTANTIATED':
-            raise NFLCMExceptionConflict("VNF instantiationState is not INSTANTIATED.")
-
-        vnf_insts.update(status=VNF_STATUS.SCALING)
-        JobUtil.add_job_status(job_id, 15, 'Nf scaling to level pre-check finish')
-        logger.info("Nf scaling to level pre-check finish")
+        return deal_vnf_action(
+            logger=logger,
+            opt_type=OPERATION_TYPE.SCALE_TO_LEVEL,
+            opt_status=VNF_STATUS.SCALING,
+            instid=instanceid,
+            req=request,
+            req_serializer=ScaleVnfToLevelRequestSerializer,
+            act_task=ScaleVnfToLevel
+        )
