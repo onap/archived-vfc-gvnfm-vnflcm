2fb3926031f690f7a922b6fe0fd064ddfe77a6b2
fujinhua <fu.jinhua@zte.com.cn>
2019-04-25 10:52:12 +0800

    Refactor codes for delete vnf ut
    Change-Id: I0a8f592afb879a17086091eae1a7b2e38a73f468
    Issue-ID: VFC-1306
    Signed-off-by: fujinhua <fu.jinhua@zte.com.cn>

--- lcm/lcm/nf/tests/test_delete_vnf.py
+++ lcm/lcm/nf/tests/test_delete_vnf.py
@@ -16,63 +16,84 @@
 from django.test import TestCase, Client
 from rest_framework import status
 
-from lcm.pub.database.models import NfInstModel, JobStatusModel, VmInstModel, NetworkInstModel, SubNetworkInstModel, \
-    PortInstModel, FlavourInstModel, StorageInstModel, NfvoRegInfoModel
+from lcm.pub.database.models import NfInstModel
+from lcm.pub.database.models import JobStatusModel
+from lcm.pub.database.models import VmInstModel
+from lcm.pub.database.models import NetworkInstModel
+from lcm.pub.database.models import SubNetworkInstModel
+from lcm.pub.database.models import PortInstModel
+from lcm.pub.database.models import FlavourInstModel
+from lcm.pub.database.models import StorageInstModel
+from lcm.pub.database.models import NfvoRegInfoModel
 from lcm.pub.utils.timeutil import now_time
 
 
 class TestNFTerminate(TestCase):
     def setUp(self):
         self.client = Client()
-        StorageInstModel.objects.create(storageid="1",
-                                        vimid="1",
-                                        resourceid="11",
-                                        insttype=0,
-                                        instid="1111",
-                                        is_predefined=1)
-        NetworkInstModel.objects.create(networkid='1',
-                                        vimid='1',
-                                        resourceid='1',
-                                        name='pnet_network',
-                                        is_predefined=1,
-                                        tenant='admin',
-                                        insttype=0,
-                                        instid='1111')
-        SubNetworkInstModel.objects.create(subnetworkid='1',
-                                           vimid='1',
-                                           resourceid='1',
-                                           networkid='1',
-                                           is_predefined=1,
-                                           name='sub_pnet',
-                                           tenant='admin',
-                                           insttype=0,
-                                           instid='1111')
-        PortInstModel.objects.create(portid='1',
-                                     networkid='1',
-                                     subnetworkid='1',
-                                     vimid='1',
-                                     resourceid='1',
-                                     is_predefined=1,
-                                     name='aaa_pnet_cp',
-                                     tenant='admin',
-                                     insttype=0,
-                                     instid='1111')
-        FlavourInstModel.objects.create(flavourid="1",
-                                        vimid="1",
-                                        resourceid="11",
-                                        instid="1111",
-                                        is_predefined=1)
-        VmInstModel.objects.create(vmid="1",
-                                   vimid="1",
-                                   resourceid="11",
-                                   insttype=0,
-                                   instid="1111",
-                                   vmname="test_01",
-                                   is_predefined=1,
-                                   operationalstate=1)
-        NfvoRegInfoModel.objects.create(nfvoid='1111',
-                                        vnfminstid='11111',
-                                        apiurl='1')
+        StorageInstModel.objects.create(
+            storageid="1",
+            vimid="1",
+            resourceid="11",
+            insttype=0,
+            instid="1111",
+            is_predefined=1
+        )
+        NetworkInstModel.objects.create(
+            networkid='1',
+            vimid='1',
+            resourceid='1',
+            name='pnet_network',
+            is_predefined=1,
+            tenant='admin',
+            insttype=0,
+            instid='1111'
+        )
+        SubNetworkInstModel.objects.create(
+            subnetworkid='1',
+            vimid='1',
+            resourceid='1',
+            networkid='1',
+            is_predefined=1,
+            name='sub_pnet',
+            tenant='admin',
+            insttype=0,
+            instid='1111'
+        )
+        PortInstModel.objects.create(
+            portid='1',
+            networkid='1',
+            subnetworkid='1',
+            vimid='1',
+            resourceid='1',
+            is_predefined=1,
+            name='aaa_pnet_cp',
+            tenant='admin',
+            insttype=0,
+            instid='1111'
+        )
+        FlavourInstModel.objects.create(
+            flavourid="1",
+            vimid="1",
+            resourceid="11",
+            instid="1111",
+            is_predefined=1
+        )
+        VmInstModel.objects.create(
+            vmid="1",
+            vimid="1",
+            resourceid="11",
+            insttype=0,
+            instid="1111",
+            vmname="test_01",
+            is_predefined=1,
+            operationalstate=1
+        )
+        NfvoRegInfoModel.objects.create(
+            nfvoid='1111',
+            vnfminstid='11111',
+            apiurl='1'
+        )
 
     def tearDown(self):
         VmInstModel.objects.all().delete()
@@ -81,26 +102,30 @@ class TestNFTerminate(TestCase):
         PortInstModel.objects.all().delete()
 
     def assert_job_result(self, job_id, job_progress, job_detail):
-        jobs = JobStatusModel.objects.filter(jobid=job_id,
-                                             progress=job_progress,
-                                             descp=job_detail)
+        jobs = JobStatusModel.objects.filter(
+            jobid=job_id,
+            progress=job_progress,
+            descp=job_detail
+        )
         self.assertEqual(1, len(jobs))
 
     def test_delete_vnf_identifier(self):
-        NfInstModel.objects.create(nfinstid='1111',
-                                   nf_name='2222',
-                                   package_id='todo',
-                                   version='',
-                                   vendor='',
-                                   netype='',
-                                   vnfd_model='',
-                                   status='NOT_INSTANTIATED',
-                                   nf_desc='',
-                                   vnfdid='',
-                                   vnfSoftwareVersion='',
-                                   vnfConfigurableProperties='todo',
-                                   localizationLanguage='EN_US',
-                                   create_time=now_time())
+        NfInstModel.objects.create(
+            nfinstid='1111',
+            nf_name='2222',
+            package_id='todo',
+            version='',
+            vendor='',
+            netype='',
+            vnfd_model='',
+            status='NOT_INSTANTIATED',
+            nf_desc='',
+            vnfdid='',
+            vnfSoftwareVersion='',
+            vnfConfigurableProperties='todo',
+            localizationLanguage='EN_US',
+            create_time=now_time()
+        )
         response = self.client.delete("/api/vnflcm/v1/vnf_instances/1111")
         self.failUnlessEqual(status.HTTP_204_NO_CONTENT, response.status_code)
         self.assertEqual(None, response.data)
