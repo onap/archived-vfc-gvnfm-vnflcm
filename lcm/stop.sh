f643d0b9eb41c020bce51c61c6e4e4ae5d40a552
fujinhua <fu.jinhua@zte.com.cn>
2019-05-07 08:55:39 +0800

    Fix bug for vnflcm stop sh
    Change-Id: I0a3e01122fa2da0e6541b114daeeeb20965da3bb
    Issue-ID: VFC-1306
    Signed-off-by: fujinhua <fu.jinhua@zte.com.cn>

--- lcm/stop.sh
+++ lcm/stop.sh
@@ -14,4 +14,4 @@
 # limitations under the License.
 
 # ps auxww | grep 'manage.py runserver 0.0.0.0:8801' | awk '{print $2}' | xargs kill -9
-ps auxww |grep 'uwsgi --http :8801 --module lcm.wsgi --master' |awk '{print $2}' |xargs kill -9
+ps auxww |grep 'uwsgi --http' |awk '{print $1}' |xargs kill -9
