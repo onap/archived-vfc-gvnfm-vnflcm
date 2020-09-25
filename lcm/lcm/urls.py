82bc77f537c6a3ecdf60dc7dfc7917bbe7c35367
dengyh <dengyuanhong@chinamobile.com>
2020-09-25 16:40:52 +0800

    Fixed duplicate oom registration for local programs
    Issue-ID: VFC-1752
    Change-Id: I8ce6a265112355d26137d19852f734d6c0c6ede0
    Signed-off-by: dengyh <dengyuanhong@chinamobile.com>

--- lcm/lcm/urls.py
+++ lcm/lcm/urls.py
@@ -29,7 +29,7 @@ urlpatterns = [
 ]
 
 # regist to MSB when startup
-if REG_TO_MSB_WHEN_START:
+if REG_TO_MSB_WHEN_START == "true":
     import json
     from lcm.pub.utils.restcall import req_by_msb
 
